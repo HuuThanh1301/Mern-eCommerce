b90583854e1a210c8d83f9f8eb549c0449406351
nhthanhdn95 <nhthanhdn95@gmail.com>
2024-05-09 08:56:09 +0930

    5. Redux Toolkit - RTK Query - State Management

---
+++ backend/data/users.js
@@ -0,0 +1,24 @@
+import bycrypt from 'bcryptjs';
+
+const users = [
+  {
+    name: 'Admin User',
+    email: 'admin@email.com',
+    password: bycrypt.hashSync('123456', 10),
+    isAdmin: true,
+  },
+  {
+    name: 'Cao Nguyen Vo',
+    email: 'caonguyen@email.com',
+    password: bycrypt.hashSync('123456', 10),
+    isAdmin: false,
+  },
+  {
+    name: 'TienDucNguyen',
+    email: 'tienduc@email.com',
+    password: bycrypt.hashSync('123456', 10),
+    isAdmin: false,
+  },
+];
+
+export default users;
