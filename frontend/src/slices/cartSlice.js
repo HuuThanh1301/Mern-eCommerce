b40e99749c14e28892e3c50946590f642bedf021
nhthanhdn95 <nhthanhdn95@gmail.com>
2024-05-31 15:46:03 +0930

    Clear local storage after user log out

--- frontend/src/slices/cartSlice.js
+++ frontend/src/slices/cartSlice.js
@@ -40,6 +40,7 @@ const cartSlice = createSlice({
       state.cartItems = [];
       return updateCart(state);
     },
+    resetCart: (state) => (state = initialState),
   },
 });
 
@@ -49,5 +50,6 @@ export const {
   saveShippingAddress,
   savePaymentMethod,
   clearCartItems,
+  resetCart,
 } = cartSlice.actions;
 export default cartSlice.reducer;
